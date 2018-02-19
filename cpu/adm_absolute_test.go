94f6fc96a20438fd780a0d20991be2d2bca123ad
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-19 20:16:10 +0100

    Test(adm_absolute): add a test for adm absolute,x

--- cpu/adm_absolute_test.go
+++ cpu/adm_absolute_test.go
@@ -32,3 +32,30 @@ func TestAbsolute(t *testing.T) {
 		}
 	}
 }
+
+func TestAbsoluteX(t *testing.T) {
+	memory := memory.New()
+	memory.SetByteBank(0xFE, 0x7E, 0x00001)
+	memory.SetByteBank(0xFF, 0x7E, 0x00002)
+
+	testCases := []struct {
+		cpu                    *CPU
+		expectedHi, expectedLo uint32
+	}{
+		{
+			cpu:        &CPU{K: 0x7E, DBR: 0x12, X: 0x000A, memory: memory},
+			expectedHi: 0x130009,
+			expectedLo: 0x130008,
+		},
+	}
+
+	for i, tc := range testCases {
+		addressHi, addressLo := tc.cpu.admAbsoluteXP()
+
+		if addressHi != tc.expectedHi {
+			t.Errorf("Test %v failed: %x %x\n", i, addressHi, tc.expectedHi)
+		} else if addressLo != tc.expectedLo {
+			t.Errorf("Test %v failed: %x %x\n", i, addressLo, tc.expectedLo)
+		}
+	}
+}
