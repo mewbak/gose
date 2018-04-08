9eb29f424bdd3ade8ae8d923e0f351e1e59e2c32
Sami Tabet <salph.tabet@gmail.com>
2018-04-08 19:52:42 +0200

    Fix(dma): added a missing dma register

--- core/memory.go
+++ core/memory.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/snes-emu/gose/ppu"
 	"github.com/snes-emu/gose/rom"
 )
@@ -66,7 +68,13 @@ func (memory Memory) GetByteBank(K uint8, offset uint16) uint8 {
 			if offset < 0x2000 {
 				return memory.wram[offset]
 			} else if 0x2133 < offset && offset < 0x2140 {
-				return memory.ppu.Registers[offset-0x2100](0)
+				res := memory.ppu.Registers[offset-0x2100](0)
+				fmt.Printf("register read: %x -> %x\n", offset, res)
+				return res
+			} else if 0x4300 < offset && offset < 0x5fff {
+				// DMA
+				fmt.Printf("DMA read: %x\n", offset)
+				return memory.cpu.GetDma(offset)
 			}
 		} else if offset < 0x8000 && ((0x6F < K && K < 0x7E) || (0xEF < K && K < 0xFE)) {
 			return memory.sram[offset]
@@ -99,6 +107,7 @@ func (memory *Memory) SetByteBank(value uint8, K uint8, offset uint16) {
 				memory.ppu.Registers[offset-0x2100](value)
 			} else if 0x4300 < offset && offset < 0x5fff {
 				// DMA
+				fmt.Printf("DMA set: %x <- \n", offset, value)
 				memory.cpu.SetDma(offset, value)
 			}
 		} else if offset < 0x8000 && ((0x6F < K && K < 0x7E) || (0xEF < K && K < 0xFE)) {
